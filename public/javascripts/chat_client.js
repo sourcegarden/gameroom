0adf8eb95073bf00a9c1ff8924cc693405a56d37
Jackie Zhang <viperking@163.com>
2015-08-09 20:20:30 +0800

    start to rewrite using approx. touch moving

--- public/javascripts/chat_client.js
+++ public/javascripts/chat_client.js
@@ -6,142 +6,36 @@
  */
 
 
-function divEscapedContentElement(message) {
-    return $('<div></div>').text(message);
-}
-
-function divSystemContentElement(message) {
-    return $('<div></div>').html('<i>' + message + '</i>');
-}
-
-function processUserInput(chatApp, socket) {
-    var message = $('#send-message').val();
-    var systemMessage;
-
-    if (message.charAt(0) == '/') {
-        systemMessage = chatApp.processCommand(message);
-        if (systemMessage) {
-            $('#messages').append(divSystemContentElement(systemMessage));
-        }
-    } else {
-        chatApp.sendMessage($('#room').text(), message);
-        $('#messages').append(divEscapedContentElement(message));
-        $('#messages').scrollTop($('#messages').prop('scrollHeight'));
-    }
-
-    $('#send-message').val('');
-}
-
 function processUserTouch(chatApp, socket)
 {
-    var leftdiv = document.getElementById("leftdiv");
-    leftdiv.ontouchstart = function(e){
+    var controller = document.getElementById("controller");
+    controller.ontouchstart = function(e){
+        $("#msg").html("");
+        $("#msg").html("<p>ontouchstart</p>");
+
         chatApp.sendTouch($('#room').text(), socket.id, 65, true);
         e.stopPropagation();
         e.preventDefault();
     };
-    leftdiv.ontouchend = function(e){
+    controller.ontouchmove = function(e){
+        $("#msg").html("");
+        $("#msg").html("<p>ontouchmove</p>");
+
+        chatApp.sendTouch($('#room').text(), socket.id, 65, false);
+        e.stopPropagation();
+        e.preventDefault();
+    };
+    controller.ontouchend = function(e){
+        $("#msg").html("");
+        $("#msg").html("<p>ontouchend</p>");
+
         chatApp.sendTouch($('#room').text(), socket.id, 65, false);
         e.stopPropagation();
         e.preventDefault();
     };
 
-    var rightdiv = document.getElementById("rightdiv");
-    rightdiv.ontouchstart = function(e){
-        chatApp.sendTouch($('#room').text(), socket.id, 68, true);
-        e.stopPropagation();
-        e.preventDefault();
-    };
-
-    rightdiv.ontouchend = function(e){
-        chatApp.sendTouch($('#room').text(), socket.id, 68, false);
-        e.stopPropagation();
-        e.preventDefault();
-    };
 
 
-    var updiv = document.getElementById("updiv");
-        updiv.ontouchstart = function(e){
-        chatApp.sendTouch($('#room').text(), socket.id, 87, true);
-            e.stopPropagation();
-            e.preventDefault();
-        };
-
-    updiv.ontouchend = function(e){
-        chatApp.sendTouch($('#room').text(), socket.id, 87, false);
-        e.stopPropagation();
-        e.preventDefault();
-    };
-
-
-    var downdiv = document.getElementById("downdiv");
-        downdiv.ontouchstart = function(e){
-        chatApp.sendTouch($('#room').text(), socket.id, 83, true);
-            e.stopPropagation();
-            e.preventDefault();
-        };
-
-    downdiv.ontouchend = function(e){
-        chatApp.sendTouch($('#room').text(), socket.id, 83, false);
-        e.stopPropagation();
-        e.preventDefault();
-    };
-
-
-    var adiv = document.getElementById("adiv");
-    adiv.ontouchstart = function(e){
-        chatApp.sendTouch($('#room').text(), socket.id, 85, true);
-        e.stopPropagation();
-        e.preventDefault();
-    };
-
-    adiv.ontouchend = function(e){
-        chatApp.sendTouch($('#room').text(), socket.id, 85, false);
-        e.stopPropagation();
-        e.preventDefault();
-    };
-
-
-    var bdiv = document.getElementById("bdiv");
-    bdiv.ontouchstart = function(e){
-        chatApp.sendTouch($('#room').text(), socket.id, 73, true);
-        e.stopPropagation();
-        e.preventDefault();
-    };
-
-    bdiv.ontouchend = function(e){
-        chatApp.sendTouch($('#room').text(), socket.id, 73, false);
-        e.stopPropagation();
-        e.preventDefault();
-    };
-
-
-    var xdiv = document.getElementById("xdiv");
-    xdiv.ontouchstart = function(e){
-        chatApp.sendTouch($('#room').text(), socket.id, 74, true);
-        e.stopPropagation();
-        e.preventDefault();
-    };
-
-    xdiv.ontouchend = function(e){
-        chatApp.sendTouch($('#room').text(), socket.id, 74, false);
-        e.stopPropagation();
-        e.preventDefault();
-    };
-
-
-    var ydiv = document.getElementById("ydiv");
-    ydiv.ontouchstart = function(e){
-        chatApp.sendTouch($('#room').text(), socket.id, 75, true);
-        e.stopPropagation();
-        e.preventDefault();
-    };
-
-    ydiv.ontouchend = function(e){
-        chatApp.sendTouch($('#room').text(), socket.id, 75, false);
-        e.stopPropagation();
-        e.preventDefault();
-    };
 
 };
 
@@ -199,15 +93,12 @@ $(document).ready(function() {
 
     socket.on('joinResult', function(result) {
         $('#room').text(result.room);
-        $('#messages').append(divSystemContentElement('Room changed.'));
     });
     socket.on('rooms', function(rooms) {
         $('#room-list').empty();
         for (var room in rooms) {
             room = room.substring(1, room.length);
-            if (room != '') {
-                $('#room-list').append(divEscapedContentElement(room));
-            }
+
         }
         $('#room-list div').click(function() {
             chatApp.processCommand('/join ' + $(this).text());
